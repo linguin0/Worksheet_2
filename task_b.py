c2f2999580656cc42e99070f773d72a14060dd0c
linguin0 <148582087+linguin0@users.noreply.github.com>
2024-10-14 10:42:44 +0000

    Autograder is not good

--- task_b.py
+++ task_b.py
@@ -1,17 +1,18 @@
 try:
     Grade = int(input("Enter your numerical grade > "))
-    match Grade:
-        case Grade if Grade >= 80:
-            print("Your grade is: A")
-        case Grade if Grade <= 79 and Grade >= 60:
-            print("Your grade is: B")
-        case Grade if Grade <= 59 and Grade >= 50:
-            print("Your grade is: C")
-        case Grade if Grade <= 49 and Grade >= 40:
-            print("Your grade is: D")
-        case Grade if Grade <= 39 and Grade >= 0:
-            print("Your grade is: F")
-        case _:
-            print("Error: Grades must be between 0 and 100")
+    if Grade < 0 or Grade > 100:
+        print("Error: Grades must be between 0 and 100")
+    else:
+        match Grade:
+            case Grade if Grade >= 80:
+                print("Your grade is: A")
+            case Grade if Grade <= 79 and Grade >= 60:
+                print("Your grade is: B")
+            case Grade if Grade <= 59 and Grade >= 50:
+                print("Your grade is: C")
+            case Grade if Grade <= 49 and Grade >= 40:
+                print("Your grade is: D")
+            case Grade if Grade <= 39 and Grade >= 0:
+                print("Your grade is: F")
 except:
     print("Error: Please enter a number")
